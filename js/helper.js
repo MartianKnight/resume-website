629a08085c083786c565e368ba056c66b795f435
MartianKnight <MartianCastle@gmail.com>
2014-10-26 08:26:15 -0400

    Add line of html for online classes to help format

--- js/helper.js
+++ js/helper.js
@@ -50,6 +50,8 @@ var HTMLschoolLocation = "<div class='location-text'>%data%</div>";
 var HTMLschoolMajor = "<em><br>Major: %data%</em>"
 
 var HTMLonlineClasses = "<h3>Online Classes</h3>";
+// Added the below line for cleaner look
+var HTMLonlineStart = "<div class='online-entry'></div>";
 var HTMLonlineTitle = "<a href='#'>%data%";
 var HTMLonlineSchool = " - %data%</a>";
 var HTMLonlineDates = "<div class='date-text'>%data%</div>";
